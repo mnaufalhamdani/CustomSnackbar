c21740896d51445e62e425520c855e20072e1f2d
dani <dani@duakelinci.co.id>
2024-02-01 15:17:59 +0700

    konfigurasi publish jitpack

--- buildSrc/src/main/java/PublishSetting.kt
+++ buildSrc/src/main/java/PublishSetting.kt
@@ -1,6 +1,6 @@
 object PublishSetting {
     const val groupId = "com.github.mnaufalhamdani"
-    const val version = "2023.02.01.0001"
+    const val version = "2023.02.01.0001"//Nanti diganti 23.01 biar lebih ringkas
 
     const val artifactIdCompose = "customsnackbar-compose"
 }
