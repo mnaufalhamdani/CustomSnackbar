fa07e83ef6fbcf5bdd0a8936956c51cfa9d716c4
dani <dani@duakelinci.co.id>
2024-02-01 14:58:49 +0700

    commit project

---
+++ buildSrc/src/main/java/PublishSetting.kt
@@ -0,0 +1,5 @@
+object PublishSetting {
+    const val groupId = "com.github.mnaufalhamdani"
+    const val artifactIdCompose = "customsnackbar-compose"
+    const val version = "2023.02.01.0001"
+}
